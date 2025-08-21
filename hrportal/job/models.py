1c5d2b09b0a70287b001223b8d136904211c425f
argentina-seo <anastasiaargen@gmail.com>
2025-08-21 16:41:22 -0300

    codex-1

--- hrportal/job/models.py
+++ hrportal/job/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Application(models.Model):
     name = models.CharField(max_length=255, blank=False, verbose_name='Полное имя')
     phone = models.CharField(max_length=100, blank=False, verbose_name='Телефон')
-    country = models.TextChoices()
+    #country = models.TextChoices()
     is_samozanat = models.BooleanField(default=False, blank=False, verbose_name='Статус самозанятого')
     need_car = models.BooleanField(default=False, blank=False, verbose_name='Нужна машина?')
 
